7a2b12b4ed0f67c39036ef7c38c703ff6c31cdcb
XuanTrung2303 <98112268+XuanTrung2303@users.noreply.github.com>
2022-09-21 17:30:01 +0700

    ee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Trung
+                    Trung a
+                    
                 </div>
 
                 <div class="links">
